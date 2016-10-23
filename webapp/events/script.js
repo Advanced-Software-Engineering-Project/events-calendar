f924777af8e6c537d7ebba9224aff625820dbfe7
bigmianhua <514066299@qq.com>
2016-10-23 16:32:57 -0400

    change search bar 2nd

--- webapp/events/script.js
+++ webapp/events/script.js
@@ -1,3 +1,10 @@
 $(function() {
     $('input[name="daterange"]').daterangepicker();
+    $('input[name="timerange"]').daterangepicker({
+        timePicker: true,
+        timePickerIncrement: 30,
+        locale: {
+            format: 'h:mm A'
+        }
+    });
 });
