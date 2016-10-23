cc369ec4be54db517b3c0318bf82d2a6873f38fc
Priscilla Wang <priscillawang@dyn-160-39-146-82.dyn.columbia.edu>
2016-10-23 19:13:25 -0400

    json parse

--- webapp/events/script.js
+++ webapp/events/script.js
@@ -11,7 +11,9 @@ $(function() {
 
 
 $(document).ready(function(){
-    var json = $.getJSON('eventdata.json');
+	// TEST JSON function
+	var j = '[{"id": 1009214592509511,"datetime": "2016-02-25T19:00:00-0500","location": "Fairchild 700","group": "Columbia Bioinformatics","title": "Bioinformatics Student Research Panel","url": "https://www.facebook.com/events/563717810449699/"},{"id": 1009214592509511,"datetime": "2016-02-25T19:00:00-0500","location": "Fairchild 700","group": "Columbia Bioinformatics","title": "Bioinformatics Student Research Panel","url": "https://www.facebook.com/events/563717810449699/"},{"id": 1009214592509511,"datetime": "2016-02-25T19:00:00-0500","location": "Fairchild 700","group": "Columbia Bioinformatics","title": "Bioinformatics Student Research Panel","url": "https://www.facebook.com/events/563717810449699/"}]';
+	var json = $.parseJSON(j);
     $(json).each(function (i, val){
     	$.each(val, function(k,v) {
     		console.log(k+" : "+ v);
