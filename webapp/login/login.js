c966f19da92e8f8854bff87a56e7d626f787ebeb
Priscilla Wang <priscillawang@dyn-160-39-146-82.dyn.columbia.edu>
2016-10-23 13:48:13 -0400

    style of login/signup

--- webapp/login/login.js
+++ webapp/login/login.js
@@ -42,6 +42,20 @@ $('.tab a').on('click', function (e) {
   
 });
 
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
 function new_account(){
     var obj ={};
     obj[firstname] = $("#firstname").val();
