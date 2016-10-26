945755780282c9409d7dfa0cc098859dd7c0daff
pw2393 <pw2393@columbia.edu>
2016-10-25 21:29:30 -0400

    updated config.py

--- server/config.py
+++ server/config.py
@@ -1,3 +1,8 @@
-# Flask config.py
-SQLALCHEMY_DATABASE_URI = "postgresql://ase4156:dbpass@localhost/eventscalendar"
+"""
+flask_sqlalchemy: config.py
+"""
+SQLALCHEMY_DATABASE_URI = "postgresql://dbpeng:dbpass@localhost/flaskr"
+#Flask-SQLAlchemy will log all the statements issued to stderr
+SQLALCHEMY_ECHO = True
+#Flask-SQLAlchemy will track modifications of objects and emit signals.
 SQLALCHEMY_TRACK_MODIFICATIONS = True
