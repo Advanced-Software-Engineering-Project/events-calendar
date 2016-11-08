0945035be9b0b26a4e75cd3e037e21ebf481c0c3
pw2393 <pw2393@columbia.edu>
2016-11-08 18:44:59 -0500

    go back bug fixed

--- server/app.py
+++ server/app.py
@@ -27,6 +27,23 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+#%%
+from flask import make_response
+from functools import wraps, update_wrapper
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+        
+    return update_wrapper(no_cache, view)
+
+
 #%% <DATABASE SCHEMA>
 """
 CODE SECTION: DATABASE SCHEMA
@@ -229,11 +246,13 @@ def addtorelationship():
 
 @app.route('/events/index.html')
 @login_required
+@nocache
 def events_check():
     return app.send_static_file('./events/index.html')
 
 @app.route('/events', methods=['GET'])
 @login_required
+@nocache
 def events_handler():
     favbuf = current_user.favorites
     print favbuf
