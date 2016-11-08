6eb171e35b2afed4ef5d5f3167df08e21070094f
ionox0 <ionox0@gmail.com>
2016-11-08 09:32:10 -0500

    testing port 80 on prod, remove CORS headers

--- server/app.py
+++ server/app.py
@@ -273,15 +273,7 @@ def refresh_event():
     #print [o.todict() for o in Event.query.all()]
     return redirect('login/index.html')
      
-    
-    
-##Temporary local development solution for CORS
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  return response
+
 
 #if __name__ == '__main__':
 #    app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
@@ -293,7 +285,7 @@ if __name__ == '__main__':
     @click.option('--threaded', is_flag=True)#RECOMMENDED
     #@click.argument('HOST', default='0.0.0.0')
     @click.argument('HOST', default='127.0.0.1')
-    @click.argument('PORT', default=5000, type=int)
+    @click.argument('PORT', default=80, type=int)
     def run(debug, threaded, host, port):
       """
       This function handles command line parameters.
