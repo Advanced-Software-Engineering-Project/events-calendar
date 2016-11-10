49ed1a059e1bc2973fe723fd67cc78c365141503
bigmianhua <514066299@qq.com>
2016-11-10 00:16:03 -0500

    test for login/signup

--- server/test_app.py
+++ server/test_app.py
@@ -14,7 +14,16 @@ import unittest
 import json
 
 from app import db, app, refresh_event
+<<<<<<< Updated upstream
 
+=======
+
+<<<<<<< HEAD
+
+#from app.models import User
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 
 class SignupTestCase(unittest.TestCase):
 
@@ -41,7 +50,23 @@ class SignupTestCase(unittest.TestCase):
             lastname='ab',
             firstname='cd'
         )))
-        assert response.status_code == 200
+        assert response.status_code == 201
+    def test_user_bademail(self):
+        response = self.app.post('/signup', data = json.dumps(dict(
+            email='abc@columbia',
+            password='passwd',
+            lastname='ab',
+            firstname='cd'
+        )))
+        assert response.status_code == 400
+    def test_exist(self):
+        response = self.app.post('/signup', data = json.dumps(dict(
+            email='abc@columbia.edu',
+            password='passwd',
+            lastname='ab',
+            firstname='cd'
+        )))
+        assert response.status_code == 409                
 
 
 
@@ -71,6 +96,24 @@ class LoginTestCase(unittest.TestCase):
             exist_password='passwd'
         )))
         assert response.status_code == 200
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    
+    def test_invalid(self):
+        response = self.app.post('/login', data = json.dumps(dict(
+            exist_email='abc@columbia.edu',
+            exist_password='wrongpassword'
+        )))
+        assert response.status_code == 401
+
+class FavoritesTestCase(unittest.TestCase):
+
+    def setUp(self):
+        #Creates a new database for the unit test to use
+    
+=======
+>>>>>>> Stashed changes
 
 
 
@@ -95,10 +138,31 @@ class EventsTestCase(unittest.TestCase):
         """
         Creates a new database for the unit test to use
         """
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
         app.config.from_pyfile('test_config.py')
         db.init_app(app)
         db.create_all()
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+        self.app = app.test_client()
+
+        user = User(active=True)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+
+
+    def tearDown(self):
+        db.drop_all()
+
+    def easylogin():
+=======
+>>>>>>> Stashed changes
         app.config['TESTING'] = True
         app.login_manager.init_app(app)
 
@@ -111,11 +175,41 @@ class EventsTestCase(unittest.TestCase):
             firstname='cd'
         )))
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
         self.app.post('/login', data = json.dumps(dict(
             exist_email='abc@columbia.edu',
             exist_password='passwd'
         )))
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    def test_set_favorites(self):
+        easylogin()
+        response = self.app.post('/favorite', data = json.dumps(dict(id = '')
+        ))
+        print(response)
+        assert response.status_code == 201
+    def test_unset_favorites(self):
+        response = self.app.delete('/favorite', data = json.dumps(dict(
+            exist_email='abc@columbia.edu',
+            exist_password='passwd'
+        )))
+        print(response)
+        assert response.status_code == 200
+    def show_favorites(self):
+        response = self.app.post('/favorite', data = json.dumps(dict(
+            exist_email='abc@columbia.edu',
+            exist_password='passwd'
+        )))
+        print(response)
+        assert response.status_code == 200
+        
+=======
+>>>>>>> Stashed changes
 
     def tearDown(self):
         db.drop_all()
@@ -144,6 +238,10 @@ class EventsTestCase(unittest.TestCase):
 
 
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     unittest.main()
