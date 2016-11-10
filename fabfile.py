9057b9351817d7d094af86bebe1e82ae59364219
ionox0 <ionox0@gmail.com>
2016-11-10 00:51:38 -0500

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,6 @@
+from fabric.api import local
+
+def deploy():
+    local("python server/test_app.py")
+    local("git add -p && git commit --allow-empty")
+    local("git push heroku master:master")
