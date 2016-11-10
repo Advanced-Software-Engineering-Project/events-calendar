b8fb0297e40b1307bf5832213b7bb18adbce8e70
ionox0 <ionox0@gmail.com>
2016-11-10 02:05:23 -0500

    revert to old clock

--- clock.py
+++ clock.py
@@ -6,25 +6,25 @@ from scraper import data_importer
 sched = BlockingScheduler()
 
 
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
-# def scheduled_job():
-#     events_scraper.get_events()
-#
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
-# def scheduled_job():
-#     data_importer.do_import()
-#
-#
-#
-
-
-@sched.scheduled_job('interval', id='get_events', minutes=2)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
 def scheduled_job():
     events_scraper.get_events()
 
-@sched.scheduled_job('interval', id='import', minutes=2)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
 def scheduled_job():
     data_importer.do_import()
+
+
+
+
+# test
+# @sched.scheduled_job('interval', id='get_events', minutes=2)
+# def scheduled_job():
+#     events_scraper.get_events()
+#
+# @sched.scheduled_job('interval', id='import', minutes=2)
+# def scheduled_job():
+#     data_importer.do_import()
 
 
 
