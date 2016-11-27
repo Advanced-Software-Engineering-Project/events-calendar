7aefa8660d41b9439a39452960abad16dd07106f
ionox0 <ionox0@gmail.com>
2016-11-27 13:33:45 -0500

    testing apscheduler in prod

--- clock.py
+++ clock.py
@@ -6,14 +6,18 @@ from scraper import data_importer
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
+@sched.scheduled_job('cron', hour=13, minute=35)
 def scheduled_job():
     events_scraper.get_events()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
+@sched.scheduled_job('cron', hour=2)
 def scheduled_job():
     data_importer.do_import()
 
+
+@sched.scheduled_job('interval', seconds=10)
+def scheduled_job():
+    print('dummy job')
 
 
 sched.start()
