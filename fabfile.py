7166ff536c998d2659622bc18623687a5c009b28
ionox0 <ionox0@gmail.com>
2016-11-27 17:38:09 -0500

    allow for tests skipping in deploy script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
 from fabric.api import local
+from distutils.util import strtobool
 
-def deploy():
-    local("python server/test_app.py")
+def deploy(skip_tests):
+    if skip_tests:
+        skip_tests = bool(strtobool(skip_tests))
+    if not skip_tests:
+        local("python server/test_app.py")
     local("git add -p && git commit --allow-empty")
     local("git push heroku master:master")
