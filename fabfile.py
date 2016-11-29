102d4bc178464349486ebfe89a66c76f50596823
ionox0 <ionox0@gmail.com>
2016-11-29 18:41:10 -0500

    add back in apscheduler to requirements file, + temporary changes to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,30 @@
-from fabric.api import local
+from fabric.api import *
+from fabric.context_managers import cd
 from distutils.util import strtobool
+
+env.use_ssh_config = True
+
 
 def deploy(skip_tests=False):
     if skip_tests:
         skip_tests = bool(strtobool(skip_tests))
     if not skip_tests:
-        local("python server/test_app.py")
+        with cd('/Users/ianjohnson/Desktop/Columbia/Advanced Software Engineering/events-calendar/server'):
+            local("pwd")
+            local("python test_app.py")
     local("git add -p && git commit --allow-empty")
     local("git push heroku master:master")
+
+
+# from fabric.api import local, run
+# from fabric.context_managers import cd
+# from distutils.util import strtobool
+#
+# def deploy(skip_tests=False):
+#     if skip_tests:
+#         skip_tests = bool(strtobool(skip_tests))
+#     if not skip_tests:
+#         with cd('/tmp'):
+#             run("python test_app.py")
+#     local("git add -p && git commit --allow-empty")
+#     local("git push heroku master:master")
