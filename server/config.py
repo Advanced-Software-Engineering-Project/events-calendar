1693663f10780098f2fb3939209d858d3a2b4da2
Peng <pw2393@columbia.edu>
2017-02-10 10:27:32 -0500

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,8 @@
 """
 flask_sqlalchemy: config.py
 """
-SQLALCHEMY_DATABASE_URI = "postgresql://ase4156:dbpass@35.185.1.150/eventscalendar"
+SQLALCHEMY_DATABASE_URI = "
+postgres://uxrqheianbljra:c21d477cbd7c186d52fba593f484e9e6862a925f3b1bd81c6f7896d06c7a8dc7@ec2-54-235-120-39.compute-1.amazonaws.com:5432/dd1hom7cbd6tgk"
 #Flask-SQLAlchemy will log all the statements issued to stderr
 SQLALCHEMY_ECHO = True
 #Flask-SQLAlchemy will track modifications of objects and emit signals.
